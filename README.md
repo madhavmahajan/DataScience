39c253042a6c413186d2ffd31331bdb897a42b9d
Madhav Mahajan <mmahajan1991@gmail.com>
2020-04-19 03:33:52 +0530

    KMeans cluster script and theory

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DataScience
-Data Science practice code snippets
+Data Science practice code snippets with theory
+
+Contents (data and python scripts) are sourced from the lectures of Udemy course - https://www.udemy.com/course/the-data-science-course-complete-data-science-bootcamp
+
+Lecture numbers in the Python scripts and indexes in data files are all referenced as is from the course to relate each and every file to the course's tutorial for better referencing.
---
+++ clustering/lecture-256-kmeans-clustering.py
@@ -0,0 +1,88 @@
+"""
+Lecture 256 - K-means clustering for 6 countries with latitude and longitude
+Lecture 258 - Clustering categorical data
+Lecture 260 - Choosing number of clusters
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+from sklearn.cluster import KMeans
+
+file_name = "./data/3.01. Country clusters.csv"
+data = pd.read_csv(file_name)
+
+print("-" * 80)
+print("Raw data from the csv file")
+print(data.head())
+
+print("-" * 80)
+print("Description of raw data")
+print(data.describe(include='all'))
+
+print("-" * 80)
+print("Scatter plot")
+plt.scatter(data['Longitude'], data['Latitude'])
+plt.xlim(-150, 150)
+plt.ylim(-90, 90)
+plt.show()
+
+# Handling categorical data # lecture 258
+# data_mapped = data.copy()
+# data_mapped = data['Language'].map({'English': 0, 'German': 1, 'French':2})
+# print(data_mapped)
+# x = data.iloc[:, 1:4]
+
+print("-" * 80)
+print("Selecting the features")
+x = data.iloc[:, 1:3]
+print(x)
+
+# Choosing number of clusters
+print("-" * 80)
+print("Calculating WCSS for possible number of clusters 1 to 7")
+wcss = []
+for i in range(1, 7):
+    kmeans = KMeans(i)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+    print("WCSS for {} cluster: {}".format(i, kmeans.inertia_))
+
+print("-" * 80)
+print("Plotting graph with WCSS on y-axis and number of clusters on x-axis")
+number_of_clusters = range(1, 7)
+plt.plot(number_of_clusters, wcss)
+plt.show()
+
+print("-" * 80)
+print("After looking at the graph, we see an elbow shape")
+print("After number of clusters is 3, there is not much change in the WCSS for rest of the clusters")
+print("which concludes, 3 cluster solution is the best fit")
+
+# Clustering
+print("-" * 80)
+print("Initiating KMeans clustering solution with 3 clusters")
+kmeans = KMeans(3)
+result = kmeans.fit(x)
+print(result)
+
+# Clustering results
+print("-" * 80)
+print("Identifying 3 clusters by fitting the data")
+identified_clusters = kmeans.fit_predict(x)
+
+print("-" * 80)
+print("Creating new table with raw data and new column marking each record with a cluster")
+data_with_clusters = data.copy()
+data_with_clusters['Cluster'] = identified_clusters
+print(data_with_clusters)
+
+print("-" * 80)
+print("Plotting data representing 3 clusters")
+plt.scatter(data_with_clusters['Longitude'], data_with_clusters['Latitude'], c=data_with_clusters['Cluster'],
+            cmap='rainbow')
+plt.xlim(-150, 150)
+plt.ylim(-90, 90)
+plt.show()
+
